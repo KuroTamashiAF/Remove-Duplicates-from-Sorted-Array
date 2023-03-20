c333561513349644b0f3ef2669609d1bf87bc14a
Fedorov <chugun777akk@mail.ru>
2023-03-20 09:34:54 +0700

    iam think

--- Source.cpp
+++ Source.cpp
@@ -5,9 +5,24 @@ using namespace std;
 
 class Solution {
 public:
-	static int removeDuplicates(vector<int>& nums) 
+	static vector<int> removeDuplicates(vector<int>& nums)
 	{
-
+		int count = 0;
+		vector<int> result;;
+		for (int i = 0; i < nums.size()-1; i++)
+		{
+			for (int j = 1; j < nums.size(); j++)
+			{
+				if (nums[i] == nums[j])
+				{
+					count++;
+					result.push_back(nums[i]);
+					
+				}
+			}
+		}
+		cout << count << "\n";
+		return result;
 	}
 };
 
@@ -17,5 +32,13 @@ int main()
 {
 	vector<int> arr1 = {1,1,2};
 	vector<int> arr2 = { 0,0,1,1,1,2,2,3,3,4 };
-	Solution::removeDuplicates(arr1);
+
+
+
+	for(int var : Solution::removeDuplicates(arr2))
+	{
+		cout << var << endl;
+	}
+
+	//Solution::removeDuplicates(arr1);
 }
