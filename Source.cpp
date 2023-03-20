b1cb58a9b36b20083de6d672bca92e7230ef5cbe
Fedorov <chugun777akk@mail.ru>
2023-03-20 19:44:31 +0700

    create source

--- Source.cpp
+++ Source.cpp
@@ -1,28 +1,46 @@
 #include <iostream>
 #include<vector>
+#include <algorithm>
 using namespace std;
 
 
 class Solution {
 public:
-	static vector<int> removeDuplicates(vector<int>& nums)
+	static int removeDuplicates(vector<int>& nums)
 	{
+		vector<int> result;
 		int count = 0;
-		vector<int> result;;
+		sort(result.begin(), result.end());
+
 		for (int i = 0; i < nums.size()-1; i++)
 		{
-			for (int j = 1; j < nums.size(); j++)
+			for (int k = i + 1; k < nums.size(); k++)
 			{
-				if (nums[i] == nums[j])
+				if (nums[i] != nums[k])
 				{
-					count++;
 					result.push_back(nums[i]);
-					
+				}
+				else
+				{
+					i++;
 				}
 			}
 		}
-		cout << count << "\n";
-		return result;
+
+		for (int var : result)
+		{
+			cout << var << endl;
+		}
+
+
+
+		
+
+
+
+
+
+		return count;
 	}
 };
 
@@ -32,13 +50,14 @@ int main()
 {
 	vector<int> arr1 = {1,1,2};
 	vector<int> arr2 = { 0,0,1,1,1,2,2,3,3,4 };
+	//vector<int> arr3 = { 23,55,1,15,66,48,75,12,56,14};
 
 
-
-	for(int var : Solution::removeDuplicates(arr2))
+	cout << Solution::removeDuplicates(arr1);
+	/*for(int var : Solution::removeDuplicates(arr2))
 	{
 		cout << var << endl;
-	}
+	}*/
 
 	//Solution::removeDuplicates(arr1);
 }
