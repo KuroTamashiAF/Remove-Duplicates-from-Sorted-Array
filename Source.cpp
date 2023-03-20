2b6f1f104307e87e529b03a48b0f49b9e77124d3
Fedorov <chugun777akk@mail.ru>
2023-03-20 09:14:05 +0700

    +

---
+++ Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+
+class Solution {
+public:
+	static int removeDuplicates(vector<int>& nums) 
+	{
+
+	}
+};
+
+
+
+int main()
+{
+	vector<int> arr1 = {1,1,2};
+	vector<int> arr2 = { 0,0,1,1,1,2,2,3,3,4 };
+	Solution::removeDuplicates(arr1);
+}
