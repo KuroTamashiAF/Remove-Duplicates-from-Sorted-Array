2ad14b94beca0803c38a05c534e37ccaebb05030
Fedorov <chugun777akk@mail.ru>
2023-03-20 20:04:16 +0700

    all tests is done

--- Source.cpp
+++ Source.cpp
@@ -8,38 +8,16 @@ class Solution {
 public:
 	static int removeDuplicates(vector<int>& nums)
 	{
-		vector<int> result;
-		int count = 0;
-		sort(result.begin(), result.end());
+		int count = 1;
 
 		for (int i = 0; i < nums.size()-1; i++)
 		{
-			for (int k = i + 1; k < nums.size(); k++)
+			if (nums[i] != nums[i + 1])
 			{
-				if (nums[i] != nums[k])
-				{
-					result.push_back(nums[i]);
-				}
-				else
-				{
-					i++;
-				}
+				nums[count] = nums[i + 1];
+				count++;
 			}
 		}
-
-		for (int var : result)
-		{
-			cout << var << endl;
-		}
-
-
-
-		
-
-
-
-
-
 		return count;
 	}
 };
@@ -48,16 +26,12 @@ public:
 
 int main()
 {
-	vector<int> arr1 = {1,1,2};
+	
 	vector<int> arr2 = { 0,0,1,1,1,2,2,3,3,4 };
-	//vector<int> arr3 = { 23,55,1,15,66,48,75,12,56,14};
-
-
-	cout << Solution::removeDuplicates(arr1);
-	/*for(int var : Solution::removeDuplicates(arr2))
+	cout << Solution::removeDuplicates(arr2);
+	for (int var : arr2)
 	{
-		cout << var << endl;
-	}*/
+		cout << var << " ";
+	}
 
-	//Solution::removeDuplicates(arr1);
 }
